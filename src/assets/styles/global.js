4387fcac0854df9dc4c240fa193ee821f631d500
MateusJSouza <mateusjdev@gmail.com>
2023-05-31 20:27:39 -0300

    style: adding global fonts and styles

---
+++ src/assets/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.gray.dark}
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    background: '#FFFFFF',
+    headerBackground: '#F4F4F4',
+    gray: {
+      dark: '#4E4E4E',
+      light: '#656363',
+    },
+    yellow: {
+      primary: '#F09035',
+      light: 'rgba(253, 215, 14, 0.2);',
+    },
+    red: {
+      primary: '#ED3237',
+      dark: '#D80000',
+    },
+  },
+}
